3a78b437e0158c4b6fe3539b2e6faf5b5703cd06
Gallucky <galbenabu1111@gmail.com>
2025-08-13 20:23:57 +0300

    Starting out with MUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    palette: { primary: { main: "#36454f" }, secondary: { main: "#252525" }, background: "" },
+    typography: {
+        h1: { fontSize: "3rem", fontWeight: 600 },
+        h2: { fontSize: "2rem", fontWeight: 600 },
+        h3: { fontSize: "1.5rem", fontWeight: 600 },
+    },
+});
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 );
--- src/pages/Main/Main.page.tsx
+++ src/pages/Main/Main.page.tsx
@@ -1,5 +1,20 @@
+import { Container, Typography } from "@mui/material";
+
 const Main = () => {
-    return <>This is the main page</>;
+    return (
+        <Container
+            sx={{
+                width: "100vw",
+                bgcolor: "tomato",
+                height: "100vh",
+                flex: "row",
+                justifyItems: "start",
+            }}>
+            <Typography variant="h1" sx={{ ml: -1, pt: 1 }}>
+                This is the main page.
+            </Typography>
+        </Container>
+    );
 };
 
 export default Main;
